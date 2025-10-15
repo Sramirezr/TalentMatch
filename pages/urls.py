d83930c2f963d478a586f08925bfc919a148b1d2
Samuel Molina <samumoe2401@gmail.com>
2025-10-15 01:51:12 -0500

    ignore

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
       path('', views.home_view, name='home'), 
       path('postulante/', views.postulante_view, name='postulante'),
+      
       path('Reclutador/', views.reclutador_view, name='reclutador'),
       path('Crear_Vacante/', views.crear_vacante_view, name='crear_vacante'),
       path('guardar_vacante/', views.crear_vacante, name='guardar_vacante'), 
